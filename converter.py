c9dea802499868959430e7c7d8ff961ebd2093b3
syafiq <syafiq.termizi@omesti.com>
2020-10-19 06:48:08 +0800

    got minio working

--- converter.py
+++ converter.py
@@ -1,7 +1,11 @@
+import os
 from datetime import datetime
 
 import pypandoc
 from flask import Flask, request
+from minio import Minio
+from minio.error import ResponseError, BucketAlreadyOwnedByYou, BucketAlreadyExists
+
 
 app = Flask(__name__)
 
@@ -11,8 +15,25 @@ def index():
     text_to_convert = request.get_json()["text"]
 
     output_file_name = f"{int(datetime.now().timestamp())}.odt"
+    full_output_file_name_path = f"temp/{output_file_name}"
+    pypandoc.convert_text(
+        text_to_convert, "odt", "html", outputfile=full_output_file_name_path
+    )
 
-    pypandoc.convert_text(text_to_convert, "odt", "html", outputfile=output_file_name)
+    minio_client = Minio(
+        "question_storage:9000",
+        access_key=os.environ.get("MINIO_ACCESS_KEY"),
+        secret_key=os.environ.get("MINIO_SECRET_KEY"),
+        secure=False,
+    )
 
-    # TODO: upload output file to storage server
+    try:
+        minio_client.make_bucket("questions")
+    except BucketAlreadyOwnedByYou as err:
+        pass
+    except BucketAlreadyExists as err:
+        pass
+
+    minio_client.fput_object("questions", output_file_name, full_output_file_name_path)
+
     return {"file": output_file_name}
