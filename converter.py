961fcafe9ce851e50af2c4db61baf008f06cbda8
syafiq <syafiq.termizi@omesti.com>
2020-10-19 04:50:13 +0800

    first commit

---
+++ converter.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+import pypandoc
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def index():
+    """
+    string of html => ODT (stored in local FS) => upload to a storage service and get url ?
+    """
+    text_to_convert = request.get_json()["text"]
+    output_file_name = f"{int(datetime.now().timestamp())}.odt"
+    pypandoc.convert_text(text_to_convert, "odt", "html", outputfile=output_file_name)
+    # TODO: upload output file to storage server
+    return {"file": output_file_name}
