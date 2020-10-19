99b0de0dc32f449b366da15fe0cfb5647a2b17c1
syafiq <syafiq.termizi@omesti.com>
2020-10-19 17:22:51 +0800

    changes

--- converter.py
+++ converter.py
@@ -15,25 +15,25 @@ def index():
     text_to_convert = request.get_json()["text"]
 
     output_file_name = f"{int(datetime.now().timestamp())}.odt"
-    full_output_file_name_path = f"temp/{output_file_name}"
+    full_output_file_path = f"temp/{output_file_name}"
     pypandoc.convert_text(
-        text_to_convert, "odt", "html", outputfile=full_output_file_name_path
+        text_to_convert, "odt", "html", outputfile=full_output_file_path
     )
 
+    bucket_name = os.environ.get("QUESTION_BUCKET_NAME")
     minio_client = Minio(
-        "question_storage:9000",
+        os.environ.get("QUESTION_STORAGE_HOST"),
         access_key=os.environ.get("MINIO_ACCESS_KEY"),
         secret_key=os.environ.get("MINIO_SECRET_KEY"),
         secure=False,
     )
 
     try:
-        minio_client.make_bucket("questions")
+        minio_client.make_bucket(bucket_name)
     except BucketAlreadyOwnedByYou as err:
         pass
     except BucketAlreadyExists as err:
         pass
 
-    minio_client.fput_object("questions", output_file_name, full_output_file_name_path)
-
-    return {"file": output_file_name}
+    minio_client.fput_object(bucket_name, output_file_name, full_output_file_path)
+    return {"file": minio_client.presigned_get_object(bucket_name, output_file_name)}
